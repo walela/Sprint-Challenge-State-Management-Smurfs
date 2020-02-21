8ebc49c7fbfe2ddc11eb0f7c929e47c223b60987
Austin Walela <austinmasibo@gmail.com>
2020-02-21 12:04:29 +0300

    Initialized providers and created action types

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react'
+import { ThemeProvider } from '@chakra-ui/core'
+import { Provider } from 'react-redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+
+const store = createStore()
 class App extends Component {
   render() {
     return (
-      <div className='App'>
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-      </div>
+      <ThemeProvider>
+        <Provider store={store}>
+          <div className='App'>
+            <h1>SMURFS! 2.0 W/ Redux</h1>
+          </div>
+        </Provider>
+      </ThemeProvider>
     )
   }
 }
